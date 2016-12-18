0e8b3a09e43bade4bb9dcc8e7b8328bdd7a4710e
Rahul khanna <rahul.khanna@nyu.edu>
2016-12-18 02:05:14 -0500

    Processing of data for auxillary view

--- src/main/java/com/newsvisualizer/visualization/beans/AccernData.java
+++ src/main/java/com/newsvisualizer/visualization/beans/AccernData.java
@@ -11,34 +11,27 @@ public class AccernData implements Comparable<AccernData> {
     private final String story_id;
     private final Date harvested_at;
     private final String entity_name;
-    private final String entity_ticker;
     private final String entity_sector;
-    private final String article_sentiment;
     private final String story_name;
-    private final String story_sentiment;
     private final int story_volume;
-    private final int event_author_rank;
     private final String source_name;
     private String shapeAssigned = "Triangle";
     private final int overall_source_rank;
     private int sourceScore = 1;
     private int sourceRank = 0;
 
-    public AccernData(String article_id, String story_id, Date harvested_at, String entity_name, String entity_ticker, String entity_sector, String article_sentiment, String story_name, String story_sentiment, int story_volume, int event_author_rank, String source_name, int overall_source_rank) {
+    public AccernData(String article_id, String story_id, Date harvested_at, String entity_name, String entity_sector, String story_name, int story_volume, String source_name, int overall_source_rank) {
         this.article_id = article_id;
         this.story_id = story_id;
         this.harvested_at = harvested_at;
         this.entity_name = entity_name;
-        this.entity_ticker = entity_ticker;
         this.entity_sector = entity_sector;
-        this.article_sentiment = article_sentiment;
         this.story_name = story_name;
-        this.story_sentiment = story_sentiment;
         this.story_volume = story_volume;
-        this.event_author_rank = event_author_rank;
         this.source_name = source_name;
         this.overall_source_rank = overall_source_rank;
     }
+
 
     public String getArticle_id() {
         return article_id;
@@ -56,16 +49,9 @@ public class AccernData implements Comparable<AccernData> {
         return entity_name;
     }
 
-    public String getEntity_ticker() {
-        return entity_ticker;
-    }
 
     public String getEntity_sector() {
         return entity_sector;
-    }
-
-    public String getArticle_sentiment() {
-        return article_sentiment;
     }
 
     public String getStory_name() {
@@ -79,29 +65,18 @@ public class AccernData implements Comparable<AccernData> {
                 ", story_id='" + story_id + '\'' +
                 ", harvested_at=" + harvested_at +
                 ", entity_name='" + entity_name + '\'' +
-                ", entity_ticker='" + entity_ticker + '\'' +
                 ", entity_sector='" + entity_sector + '\'' +
-                ", article_sentiment='" + article_sentiment + '\'' +
                 ", story_name='" + story_name + '\'' +
-                ", story_sentiment='" + story_sentiment + '\'' +
                 ", story_volume=" + story_volume +
-                ", event_author_rank=" + event_author_rank +
                 ", source_name='" + source_name + '\'' +
                 ", overall_source_rank=" + overall_source_rank +
                 '}';
-    }
-
-    public String getStory_sentiment() {
-        return story_sentiment;
     }
 
     public int getStory_volume() {
         return story_volume;
     }
 
-    public int getEvent_author_rank() {
-        return event_author_rank;
-    }
 
     public String getSource_name() {
         return source_name;
--- src/main/java/com/newsvisualizer/visualization/dboperations/QueryOperations.java
+++ src/main/java/com/newsvisualizer/visualization/dboperations/QueryOperations.java
@@ -31,10 +31,12 @@ public class QueryOperations {
 
         BasicDBObject sourceFetchQuery = new BasicDBObject();
         sourceFetchQuery.put("story_volume", new BasicDBObject("$gte", threshold));
-        List<String> source_name = collection.distinct("source_name", sourceFetchQuery);
+
+
+        List<String> story_id = collection.distinct("story_id", sourceFetchQuery);
         BasicDBObject query = new BasicDBObject();
         query.put("entity_sector", sector);
-        query.put("source_name", new BasicDBObject("$in", source_name));
+        query.put("story_id", new BasicDBObject("$in", story_id));
         query.put("overall_source_rank", new BasicDBObject("$gt", sourceRank));
         DBCursor cursor = collection.find(query);
         cursor.sort(new BasicDBObject("story_id", 1));
@@ -44,10 +46,10 @@ public class QueryOperations {
         while (cursor.hasNext()) {
             DBObject object = cursor.next();
             AccernData data = new AccernData((String) object.get("article_id"), (String) object.get("story_id"),
-                    (Date) object.get("harvested_at"), (String) object.get("entity_name"), (String) object.get("entity_ticker"),
-                    (String) object.get("entity_sector"), (String) object.get("article_sentiment"), (String) object.get("story_name"),
-                    (String) object.get("story_sentiment"), (int) object.get("story_volume"), (int) object.get("event_author_rank"),
-                    (String) object.get("source_name"), (int)object.get("overall_source_rank"));
+                    (Date) object.get("harvested_at"), (String) object.get("entity_name"),
+                    (String) object.get("entity_sector"), (String) object.get("story_name"),
+                    (int) object.get("story_volume"),
+                    (String) object.get("story_id"), (int) object.get("overall_source_rank"));
 //            System.out.println("data.getSource_name() = " + data.getSource_name());
             dataToReturn.add(data);
 //            System.out.println("data.toString() = " + data.toString());
